b6e485dcdeb2c11da91ef98b6d925144ddb621c0
Osiris8 <miganosiris8@gmaaail.com>
2024-09-21 12:30:39 +0200

    add Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Hero />
+      <Navbar />
+
       <Features />
     </div>
   );
