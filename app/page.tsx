ae2564577beaebb7562d5930b2182b9d37aec742
Osiris8 <miganosiris8@gmaaail.com>
2024-10-02 16:57:33 +0200

    update Cta component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Cta from "@/components/cta";
+import Cta from "@/components/Cta";
 import { Feature } from "@/components/Feature";
 import { Footer } from "@/components/Footer";
 import { Hero } from "@/components/Hero";
--- components/cta.tsx
+++ components/cta.tsx
@@ -7,9 +7,9 @@ export default function Cta() {
     <section className="bg-indigo-700 dark:bg-indigo-900">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
-          <span className="block">Ready to dive in?</span>
+          <span className="block">Prêt à vous lancer ?</span>
           <span className="block text-indigo-200">
-            Start your free trial today.
+            Commencez votre essai gratuit.
           </span>
         </h2>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
@@ -18,7 +18,7 @@ export default function Cta() {
               href="#"
               className="inline-flex items-center justify-center px-5 py-3 border border-neutral-200 border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 dark:border-neutral-800"
             >
-              Get started
+              S&#39;inscrire
             </a>
           </div>
           <div className="ml-3 inline-flex rounded-md shadow">
@@ -26,7 +26,7 @@ export default function Cta() {
               href="#"
               className="inline-flex items-center justify-center px-5 py-3 border border-neutral-200 border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 dark:border-neutral-800"
             >
-              Learn more
+              Se connecter
               <ArrowRight className="ml-2 -mr-1 h-5 w-5" aria-hidden="true" />
             </a>
           </div>
